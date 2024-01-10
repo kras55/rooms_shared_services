ecbfaa9ca43601fe37821731a6f21d8bf693ce84
kras55 <dkras5577@gmail.com>
2024-01-10 22:12:54 +0200

    dynamodb client created

---
+++ docker/main/entry_point.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+if [ ! -z "${UNIT_TEST}" ]; then
+  echo "Starting unit tests ..."
+  exec /usr/local/bin/python -m pytest -vv tests/unit
+elif [ ! -z "${INTEGRATION_TEST}" ]; then
+  echo "Starting integration tests ..."
+  exec /usr/local/bin/python -m pytest -vv tests/integration
+else
+  exec /usr/local/bin/python -m rooms_shared_services.src.main
+fi
